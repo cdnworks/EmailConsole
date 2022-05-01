fa46b431805b327cee72e127c484bad4fd0fc4f0
cdnworks <chrisdnielson@gmail.com>
2022-04-30 21:13:12 -0700

    Initial commit

---
+++ EmailConsole/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/* This console is for running and testing EmailHandler.dll
+ * 
+ */
+using EmailHandler;
+using System.Configuration;
+
+
+//pull inputs from App.config
+string host = ConfigurationManager.AppSettings.Get("host");
+int port = Convert.ToInt32(ConfigurationManager.AppSettings.Get("port"));
+string userName = ConfigurationManager.AppSettings.Get("userName");
+string userEmail = ConfigurationManager.AppSettings.Get("userEmail");
+string userPass = ConfigurationManager.AppSettings.Get("userPass");
+
+
+//this section is temporary until I can reliably send email and complete the other objectives in the project.
+//pull inputs from console input
+
+Console.Write("Destination Email: ");
+string recipientEmail = Console.ReadLine();
+
+string subject = "Test";
+string messageBody = "This is a test message";
+
+
+
+Console.WriteLine("Creating email object, and sending email.");
+
+
+Email email = new Email();
+//SendEmail() is asynchronous, but it's a 'fire and forget' method.
+email.SendEmail(host, port, userName, userEmail, userPass, recipientEmail, subject, messageBody);
+
+
+Console.WriteLine("SendMail() called, main continues.");
+Console.ReadKey();
+
+
+
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# EmailConsole
+ A test/demonstration console app for my EmailHandler DLL
