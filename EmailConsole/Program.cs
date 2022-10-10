c1a5e78cfe8cc10eaf38ea84f71d1e64c1262a25
Chris Nielson <chrisdnielson@gmail.com>
2022-10-09 19:06:57 -0700

    Fixed typos and formatting

--- EmailConsole/Program.cs
+++ EmailConsole/Program.cs
@@ -6,8 +6,7 @@
 using EmailHandler;
 using System.Configuration;
 
-
-//pull inputs from App.config
+//pull Email properties from App.config
 string host = ConfigurationManager.AppSettings.Get("host");
 int port = Convert.ToInt32(ConfigurationManager.AppSettings.Get("port"));
 string userName = ConfigurationManager.AppSettings.Get("userName");
@@ -15,9 +14,8 @@ string userEmail = ConfigurationManager.AppSettings.Get("userEmail");
 string userPassword = ConfigurationManager.AppSettings.Get("userPass");
 
 
-//this section is temporary until I can reliably send email and complete the other objectives in the project.
-//pull inputs from console input
 
+//get some info from the user
 Console.Write("Destination Email: ");
 string recipientEmail = Console.ReadLine();
 
@@ -25,12 +23,10 @@ string messageSubject = "Test";
 string messageBody = "This is a test message";
 
 
-
 Console.WriteLine("Creating email object, and sending email.");
 
-
 Email email = new Email(host, port, userName, userEmail, userPassword, recipientEmail, messageSubject, messageBody);
-//SendEmail() is asynchronous, but it's a 'fire and forget' method.
+
 email.SendEmail();
 
 
